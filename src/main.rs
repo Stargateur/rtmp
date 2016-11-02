70b26785ac813bcbbe8af13959f60db3e977ea49
Stargateur <antoine.plaskowski@epitech.eu>
2016-11-02 19:49:03 +0100

    alloc and init

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ extern crate rtmp_sys;
 
 fn main() {
   unsafe {
-      println!("{:?}", rtmp_sys::rtmp::RTMP_LibVersion())
+      use rtmp_sys::rtmp;
+      println!("{:?}", rtmp::RTMP_LibVersion());
+
+      let rtmp = rtmp::RTMP_Alloc();
+      if (!rtmp.is_null()) {
+        rtmp::RTMP_Init(rtmp);
+  }
+//      rtmp::RTMP_Connect(rtmp, std::ptr::null_mut());
   }
 }
