ab27fa909f29ce54f832addec151b863a4386765
Stargateur <antoine.plaskowski@epitech.eu>
2016-11-03 05:51:36 +0100

    problem with as raw fd

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,60 @@
 extern crate rtmp_sys;
 
-fn main() {
-  unsafe {
-      use rtmp_sys::rtmp;
-      println!("{:?}", rtmp::RTMP_LibVersion());
+// use std::os::unix::io::AsRawFd;
 
-      let rtmp = rtmp::RTMP_Alloc();
-      if (!rtmp.is_null()) {
-        rtmp::RTMP_Init(rtmp);
-  }
-//      rtmp::RTMP_Connect(rtmp, std::ptr::null_mut());
-  }
+fn handle_client(stream: std::net::TcpStream) -> () {
+    println!("{:?}, {:?}", stream.peer_addr(), stream.local_addr());
+    unsafe {
+        rtmp_sys::log::RTMP_LogSetLevel(rtmp_sys::log::RTMP_LogLevel::RTMP_LOGINFO);
+        let rtmp = rtmp_sys::rtmp::RTMP_Alloc();
+        rtmp_sys::rtmp::RTMP_Init(rtmp);
+        if rtmp.is_null() {
+            panic!("rtmp is null")
+        }
+        let packet: rtmp_sys::rtmp::RTMPPacket;
+        //        (*rtmp).m_sb.sb_socket = stream.as_raw_fd();
+    }
+}
+
+fn main() {
+    let version = unsafe { rtmp_sys::rtmp::RTMP_LibVersion() };
+    println!("version = {}.{}.{}",
+             version >> 16 & 0xff,
+             version >> 8 & 0xff,
+             version & 0xff);
+    let listener = std::net::TcpListener::bind("0.0.0.0:4242").unwrap();
+    let mut handle_thread = Vec::new();
+    //  let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let builder = std::thread::Builder::new().name("handle_client".into());
+                //        let thread_tx = tx.clone();
+
+                match builder.spawn(move || handle_client(stream)) {
+                    Ok(thread) => {
+                        handle_thread.push(thread);
+                    }
+                    Err(e) => {
+                        println!("{:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        }
+    }
+
+    for thread in handle_thread {
+        match thread.join() {
+            Ok(_) => {
+                return;
+            }
+            Err(_) => {
+                return;
+            }
+        }
+    }
 }
